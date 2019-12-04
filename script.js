32ca73a5bd27edb4dbb9b47a9808a4365b5d7341
keitaroryu <keitaroryu@gmail.com>
2019-12-04 10:07:56 -0600

    Added code to fetch Mission Target and Planet data from JSON.

--- script.js
+++ script.js
@@ -50,14 +50,32 @@ window.addEventListener("load", function(){
          if(fuelLevelText.value >= 10000 && cargoMassText.value <=10000){
             launchStatus.innerHTML = "Shuttle is ready for launch";
             launchStatus.style.color = "green";
+
+            fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response){
+               response.json().then(function(json){
+                  const missionTarget = document.getElementById("missionTarget");
+                  let index = 0;
+
+                  missionTarget.innerHTML = `
+                     <h2>Mission Destination</h2>
+                     <ol>
+                        <li>Name: ${json[index].name}</li>
+                        <li>Diameter: ${json[index].diameter}</li>
+                        <li>Star: ${json[index].star}</li>
+                        <li>Distance from Earth: ${json[index].distance}</li>
+                        <li>Number of Moons: ${json[index].moons}</li>
+                     </ol>
+                     <img src="${json[index].image}">
+                  `;
+               });
+            });
+
             event.preventDefault();
          }
       }
-
-
-
    });
 });
+
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
 <ol>
