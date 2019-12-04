2197a21b189221c5b90aa78818e5e8bcc002b155
keitaroryu <keitaroryu@gmail.com>
2019-12-04 07:47:26 -0600

    Added form validation.  Ensure user entered data in all fields. Ensure only numbers allowed in fuel level and cargo mass.

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 // Write your JavaScript code here!
+window.addEventListener("load", function(){
+   let form = document.querySelector("form");
+   form.addEventListener("submit", function(){
+      let pilotNameText = document.querySelector("input[name=pilotName]");
+      let copilotNameText = document.querySelector("input[name=copilotName]");
+      let fuelLevelText = document.querySelector("input[name=fuelLevel]");
+      let cargoMassText = document.querySelector("input[name=cargoMass]");
 
+      if(pilotNameText.value === "" || copilotNameText.value === "" || fuelLevelText.value === "" || 
+      cargoMassText.value === "" || isNaN(fuelLevelText.value) || isNaN(cargoMassText.value)){
+         alert('All fields required. Only numbers in "Fuel Level" and "Cargo Mass" fields.');
+         event.preventDefault();
+      }   
+   });
+});
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
 <ol>
