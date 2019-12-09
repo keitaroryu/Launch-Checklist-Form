45f882bd0079835333cd76fbe199552112d5ca18
keitaroryu <keitaroryu@gmail.com>
2019-12-09 17:56:47 -0600

    Refactored script to remove Nested IF Conditionals. Mission Data is removed if later entered non-mission ready fuel/cargo data.

--- script.js
+++ script.js
@@ -17,62 +17,64 @@ window.addEventListener("load", function(){
       if(pilotNameText.value === "" || copilotNameText.value === "" || fuelLevelText.value === "" || 
       cargoMassText.value === ""){
          alert('All fields are required.');
-         event.preventDefault();
-
-      } else if(isNaN(fuelLevelText.value) || isNaN(cargoMassText.value)){
-         alert('Make sure to enter valid information for each field!');
-         event.preventDefault();
-
-      } else {
-         pilotStatus.innerHTML = `Pilot ${pilotNameText.value} is ready for launch`;
-         copilotStatus.innerHTML = `Co-pilot ${copilotNameText.value} is ready for launch`;
-   
-         if(fuelLevelText.value < 10000){
-            faultyItems.style.visibility = "visible";
-            fuelStatus.innerHTML = `Fuel level too low for launch`;
-            launchStatus.innerHTML = "Shuttle not ready for launch";
-            launchStatus.style.color = "red";
-            event.preventDefault();
-         } else {
-            fuelStatus.innerHTML = `Fuel level high enough for launch`;
-         }
-
-         if(cargoMassText.value >10000){
-            faultyItems.style.visibility = "visible";
-            cargoStatus.innerHTML = `Cargo mass too high for launch`;
-            launchStatus.innerHTML = "Shuttle not ready for launch";
-            launchStatus.style.color = "red";
-            event.preventDefault();
-         } else {
-            cargoStatus.innerHTML = `Cargo mass low enough for launch`;
-         }
-
-         if(fuelLevelText.value >= 10000 && cargoMassText.value <=10000){
-            launchStatus.innerHTML = "Shuttle is ready for launch";
-            launchStatus.style.color = "green";
-
-            fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response){
-               response.json().then(function(json){
-                  const missionTarget = document.getElementById("missionTarget");
-                  let index = Math.floor(Math.random()*6); //Bonus: Added Randomizer to Planet Fetch
-
-                  missionTarget.innerHTML = `
-                     <h2>Mission Destination</h2>
-                     <ol>
-                        <li>Name: ${json[index].name}</li>
-                        <li>Diameter: ${json[index].diameter}</li>
-                        <li>Star: ${json[index].star}</li>
-                        <li>Distance from Earth: ${json[index].distance}</li>
-                        <li>Number of Moons: ${json[index].moons}</li>
-                     </ol>
-                     <img src="${json[index].image}">
-                  `;
-               });
-            });
-
-            event.preventDefault();
-         }
+         return event.preventDefault();
       }
+      
+      if(isNaN(fuelLevelText.value) || isNaN(cargoMassText.value)){
+         alert('Make sure to enter valid information for each field!');
+         return event.preventDefault();
+      } 
+   
+      pilotStatus.innerHTML = `Pilot ${pilotNameText.value} is ready for launch`;
+      copilotStatus.innerHTML = `Co-pilot ${copilotNameText.value} is ready for launch`;
+
+      if(fuelLevelText.value < 10000){
+         faultyItems.style.visibility = "visible";
+         fuelStatus.innerHTML = `Fuel level too low for launch`;
+         launchStatus.innerHTML = "Shuttle not ready for launch";
+         launchStatus.style.color = "red";
+         missionTarget.innerHTML = ``;
+         event.preventDefault();
+      } else {
+         fuelStatus.innerHTML = `Fuel level high enough for launch`;
+      }
+
+      if(cargoMassText.value >10000){
+         faultyItems.style.visibility = "visible";
+         cargoStatus.innerHTML = `Cargo mass too high for launch`;
+         launchStatus.innerHTML = "Shuttle not ready for launch";
+         launchStatus.style.color = "red";
+         missionTarget.innerHTML = ``;
+         event.preventDefault();
+      } else {
+         cargoStatus.innerHTML = `Cargo mass low enough for launch`;
+      }
+
+      if(fuelLevelText.value >= 10000 && cargoMassText.value <=10000){
+         launchStatus.innerHTML = "Shuttle is ready for launch";
+         launchStatus.style.color = "green";
+
+         fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response){
+            response.json().then(function(json){
+               const missionTarget = document.getElementById("missionTarget");
+               let index = Math.floor(Math.random()*6); //Bonus: Added Randomizer to Planet Fetch
+
+               missionTarget.innerHTML = `
+                  <h2>Mission Destination</h2>
+                  <ol>
+                     <li>Name: ${json[index].name}</li>
+                     <li>Diameter: ${json[index].diameter}</li>
+                     <li>Star: ${json[index].star}</li>
+                     <li>Distance from Earth: ${json[index].distance}</li>
+                     <li>Number of Moons: ${json[index].moons}</li>
+                  </ol>
+                  <img src="${json[index].image}">
+               `;
+            });
+         });
+         return event.preventDefault();
+      }
+      
    });
 });
 
